fa6537d69b9e65e68fc75e7886dc66a97dca45b5
kush34 <chattkush34@gmail.com>
2025-04-20 14:31:12 +0530

    upload profile picture in Settings page and updated new User profiles in UserCard component

--- frontend/src/Components/UserCard.jsx
+++ frontend/src/Components/UserCard.jsx
@@ -9,7 +9,7 @@ const UserCard = ({user}) => {
   return (
     <div onClick={()=>handleClick(user.username)} className='flex items-center pb-3 hover:bg-zinc-200 ease-in duration-150 rounded cursor-pointer'>
         <div className="ml-2 userimg mt-2">
-            <img className='w-12 h-12 rounded-full' src="https://placehold.co/400x400" alt="" />
+            <img className='w-12 h-12 rounded-full' src={user?.profilePic} alt="" />
         </div>
         <div className='mx-2'>
             <div className="userName text-xl font-medium">
--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -1,13 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useUser } from '../Store/UserContext';
 import {useNavigate} from "react-router-dom"
+import api from '../utils/axiosConfig';
 const Settings = () => {
     const {user} = useUser();
     const navigate = useNavigate();
+    const fileInputRef = useRef();
     const logOut = ()=>{
         localStorage.setItem("token", "");
         navigate("/login")
     }
+    const handleUpload = async (e) => {
+        const formData = new FormData();
+        formData.append("file", e.target.files[0]);
+      
+        try {
+          const res = await api.post("/user/profilepic", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          });
+          console.log("Uploaded URL:", res.data.url);
+          // Save this URL in user profile or video DB
+        } catch (err) {
+          console.error(err);
+        }
+      };
+      const triggerFileInput = () => {
+        fileInputRef.current.click();
+      };
+      
   return (
     <div>
         <div className="head flex items-center justify-between">
@@ -24,8 +46,9 @@ const Settings = () => {
                 onError={(e) => { e.target.src = "836.jpg"; }}
                 alt="User profile"
             />
-            <div className="edit cursor-pointer">
-                Edit
+            <div className="edit m-5 cursor-pointer">
+            <input type="file" className='hidden' onChange={handleUpload}  ref={fileInputRef}/>
+            <button onClick={triggerFileInput} className='cursor-pointer hover:bg-transparent hover:text-black ease-in duration-150 border-2 border-black bg-black px-3 py-2 rounded font-bold text-white'>Change</button>
             </div>
             </div>
             <div className="username flex flex-col text-xl items-center justify-center ">
@@ -39,7 +62,7 @@ const Settings = () => {
                 </span>
             </div>
             <div className="flex justify-center m-5 " onClick={logOut}>
-                <button className="cursor-pointer font-bold text-white bg-black px-4 py-2 rounded hover:text-red-500 hover:border-2 border-red-500 hover:bg-transparent">
+                <button className="cursor-pointer font-bold text-white bg-black px-4 py-2 rounded hover:text-red-500 hover:border-2 border-red-500 hover:bg-transparent ease-in duration-150">
                     Log Out
                 </button>
             </div>
