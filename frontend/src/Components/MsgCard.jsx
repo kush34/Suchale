e498f6971aca55b3fcfa0421f5dbc00faf342c39
kush34 <chattkush34@gmail.com>
2025-05-12 19:13:54 +0530

    cleaned the msgCard UI

--- frontend/src/Components/MsgCard.jsx
+++ frontend/src/Components/MsgCard.jsx
@@ -10,23 +10,23 @@ const MsgCard = ({msg}) => {
         return formatted;
     }
   return (
-    <div className='flex gap-2 items-center '>
+    <div className=' gap-2 items-center '>
         <div className="msgContent text-xl ">
         {msg.content?.match(/\.(jpeg|jpg|gif|png|webp)$/i) ? (
             // <img src={msg.content} alt="uploaded" className="max-w-xs rounded-lg" />
             <ChatImageViewer src={msg.content}/>
         ) : (
-        <div className='flex justify-center'>
+        <div className='flex justify-center text-lg'>
             {msg.content}
         </div>
         )}
         </div>
-        <div className="flex justify-end gap-2">
-            <div className='text-sm text-zinc-500'>
+        <div className="flex text-[10px] justify-end gap-2">
+            <div className=' text-zinc-500'>
                 {getDate(msg.createdAt)}
             </div>
-            <span className='text-white text-xl'>
-                {msg.read ? <CheckCheck />:<Check /> }
+            <span className='text-white'>
+                {msg.read ? <CheckCheck size={16}/>:<Check size={16} /> }
             </span>
         </div>
     </div>
