4fa7ccbf4dfc0009ddaf2f9eb9a171bdabf6953d
kush34 <chattkush34@gmail.com>
2025-05-12 18:20:44 +0530

    add loader to home screen

---
+++ frontend/src/loaders/Loader1.jsx
@@ -0,0 +1,12 @@
+import Reat from "react";
+import "./loader1.css";
+const Loader1 = ()=>{
+	return (
+		<div className="main">
+		<div className = "spinner w-12 h-12 rounded-full border-2">
+			
+		</div>
+		</div>
+	)
+}
+export default Loader1;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,17 +5,21 @@ import UserList from '../Components/UserList'
 import { useNavigate } from 'react-router-dom'
 import socket from '../utils/socketService';
 import { ChatContext } from '../Store/ChatContext';
+import Loader1 from "../loaders/Loader1.jsx"
 const Home = () => {
   const navigate = useNavigate();
   const [userChatList,setUserChatList] = useState([]);
+  const [loading,setLoading] = useState(false);
   const {chat} = useContext(ChatContext);
   //list of contacts of user
   
   const getChatList = async ()=>{
     try{
+	setLoading(true);	
       const response = await api.get('/user/userList');
       // console.log(response.data);
-      setUserChatList(response.data.response);
+        setUserChatList(response.data.response);
+     setLoading(false);
     }
     catch(err){
       console.log(err.message);
@@ -52,7 +56,9 @@ const Home = () => {
       socket.off("friendOffline", friendOnline);
     };
   }, []);
-  
+  if(loading){
+	return ( <Loader1/>);
+  }
   return (
     // <div className='flex h-screen bg-zinc-300'>
     <div className={`md:flex h-screen bg-zinc-300 overflow-none`}> 
