722f3d2134839075da0b367d1c4eca4d66089ee9
kush34 <chattkush34@gmail.com>
2025-10-31 18:20:09 +0530

    added:Message tests

--- backend/tests/users.test.js
+++ backend/tests/users.test.js
@@ -7,6 +7,17 @@ beforeAll(async () => {
 });
 
 let jwt_token;
+let fromUser = {
+    username: 'TestUser',
+    email: 'test@example.com',
+    password: 'password123'
+};
+let toUser = {
+    username: 'TestUser2',
+    email: 'test2@example.com',
+    password: 'password123'
+};
+
 describe('User Routes', () => {
     it('should register a new user', async () => {
         const res = await request(server)
@@ -28,6 +39,7 @@ describe('User Routes', () => {
             });
         expect(res2.statusCode).toBe(200);
         console.log(res.body)
+        fromUser = (res2.body)
         const res1 = await request(server)
             .post('/user/create')
             .send({
@@ -35,6 +47,7 @@ describe('User Routes', () => {
                 email: 'test@example.com',
                 password: 'password123'
             });
+        fromUser = (res1.body)
 
         console.log("second request", res1.body)
         expect(res1.statusCode).toBe(401);
@@ -74,11 +87,12 @@ describe('User Routes', () => {
             .post('/user/search')
             .set('Authorization', `Bearer ${jwt_token}`)
             .send({
-                query: 'TestUser',
+                query: 'TestUser2',
             });
 
-        console.log(res.body)
+        console.log(res.body[0])
         expect(res.statusCode).toBe(200);
+        toUser = res.body[0]
     });
     it('GET User Profile / INFO', async () => {
         const res = await request(server)
@@ -87,6 +101,7 @@ describe('User Routes', () => {
 
         console.log(res.body)
         expect(res.statusCode).toBe(200);
+        fromUser = res.body
     });
     it('POST User Add contact', async () => {
         const res = await request(server)
@@ -126,7 +141,60 @@ describe('User Routes', () => {
 
 });
 
+let sentMsg;
+let groupName = 'NewGroup';
+let createdGroup;
+describe('Message Routes', () => {
+    it('POST /send : create Message, error for not all required fields', async () => {
+        const res = await request(server)
+            .post('/message/send')
+            .set('Authorization', `Bearer ${jwt_token}`)
+        expect(res.statusCode).toBe(400)
+    });
 
+    it('POST /send : create Message', async () => {
+        const res = await request(server)
+            .post('/message/send')
+            .set('Authorization', `Bearer ${jwt_token}`)
+            .send({ content: 'Dil mange more (..)', toUser: toUser.username, isGroup: false, groupId: null })
+        console.log(res.body)
+        expect(res.statusCode).toBe(200)
+        expect(res.body.content).toBe('Dil mange more (..)')
+        expect(res.body.fromUser).toBe(fromUser.username)
+        expect(res.body.toUser).toBe(toUser.username)
+        sentMsg = res.body
+    });
+    it('POST /createGroup : create new group', async () => {
+        const res = await request(server)
+            .post('/message/createGroup')
+            .set('Authorization', `Bearer ${jwt_token}`)
+            .send({ name: groupName, users: [fromUser._id, toUser._id] })
+        expect(res.statusCode).toBe(200)
+        expect(res.body.newGroup.name).toBe(groupName)
+        console.log(res.body)
+        createdGroup = res.body.newGroup
+    });
+    it('POST /getMessages : fetch latest Messages', async () => {
+        const res = await request(server)
+            .post('/message/getMessages')
+            .set('Authorization', `Bearer ${jwt_token}`)
+            .send({ toUser: toUser.username, groupId: null, isGroup: null })
+        expect(res.statusCode).toBe(200)
+        console.log(res.body)
+        expect(res.body.messages.length).toBeGreaterThan(0)
+        expect(res.body.messages[0]).toStrictEqual(sentMsg)
+        expect(res.body.hasMore).toBe(false)
+    });
+    it('GET /getMembers/:groupId : fetch Group Members', async () => {
+        const res = await request(server)
+            .post(`/message/getMembers/${createdGroup._id}`)
+            .set('Authorization', `Bearer ${jwt_token}`)
+            .send()
+        expect(res.statusCode).toBe(200)
+        console.log(res.body)
+    });
+
+});
 afterAll(async () => {
     await mongoose.connection.db.dropDatabase();
     await mongoose.connection.close();
