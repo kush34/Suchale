3206eba915c5c1e7a8445dbe5a9bf9e1e474024d
kush34 <chattkush34@gmail.com>
2025-11-12 15:21:17 +0530

    Deployment Changes:port fix for render

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -138,7 +138,8 @@ export { io };
 export default server;
 
 if (process.env.NODE_ENV !== 'test') {
-  server.listen(3000, () => {
-    console.log('server running at http://localhost:3000');
+  const port = process.env.PORT || 3000
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
   });
 }
