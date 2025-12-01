9047fe3b1156727c3e12badaecc25593207ec710
kush34 <chattkush34@gmail.com>
2025-12-01 11:42:49 +0530

    made lower case components fldr and fix paths in home dir

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { Navbar } from "@/components/landing/home-navbar";
-import { Hero } from "@/components/landing/Hero";
+import { Hero } from "@/components/landing/hero";
 import { PhotoMasonary } from "@/components/landing/photo-marque";
-import FAQs from "@/components/landing/FAQs";
+import FAQs from "@/components/landing/faqs";
 
 const App = () => {
   const navigate = useNavigate();
