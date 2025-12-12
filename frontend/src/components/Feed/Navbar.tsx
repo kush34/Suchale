0d119bdd5ac69a894a085f65a628c4d08c554cf3
kush <chattkush34@gmail.com>
2025-12-12 20:27:13 +0530

    added profile on the appsidebar and aslo reduced theme switch time

--- frontend/src/components/Feed/Navbar.tsx
+++ frontend/src/components/Feed/Navbar.tsx
@@ -1,5 +1,7 @@
 import { Home, Inbox, Search, Settings } from "lucide-react";
 import { Link } from "react-router-dom";
+import Profile from "./Post/Profile";
+import { useUser } from "@/Store/UserContext";
 
 // Menu items.
 const items = [
@@ -26,20 +28,28 @@ const items = [
 ];
 
 export function AppSidebar() {
+  const userCtx = useUser();
+  const user = userCtx?.user;
+  if (!user) return null;
   return (
-    <div className="bg-background hidden xl:flex flex-col px-12 py-36 h-screen gap-5 border border-border">
+    <div className="bg-background hidden xl:flex flex-col px-16 py-36 h-screen gap-24 border border-border">
       <div className="text-lg md:text-xl xl:text-2xl font-bold xl:px-3 xl:py-2 tracking-wide ">
         Suchale
       </div>
-      {items.map((item) => (
-        <Link
-          className="flex gap-3 px-3 py-2 hover:scale-101 hover:text-foreground hover:bg-card rounded-xl duration-200 transition-ease-in"
-          to={item.url}
-        >
-          <item.icon />
-          {item.title}
-        </Link>
-      ))}
+      <div  className="grid gap-4">
+        {items.map((item) => (
+          <Link
+            className="flex text-xl gap-3 px-3 py-2 hover:scale-101 hover:text-foreground hover:bg-card rounded-xl duration-200 transition-ease-in"
+            to={item.url}
+          >
+            <item.icon />
+            {item.title}
+          </Link>
+        ))}
+      </div>
+      <div className="absolute bottom-10 footer">
+        <Profile className="text-lg" toLink="/settings" username={user?.username} src={user?.profilePic} />
+      </div>
     </div>
   );
 }
--- frontend/src/components/Feed/Post/Profile.tsx
+++ frontend/src/components/Feed/Post/Profile.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
+const Profile = ({
+  src,
+  username,
+  size = "w-10 h-10",
+  toLink = `/profile/${username}`,
+  className,
+}: {
+  src: string;
+  username: string;
+  size?: string;
+  toLink?: string;
+  className?: string;
+}) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      onClick={() => navigate(toLink)}
+      className={` ${className} cursor-pointer flex items-center gap-2 text-zinc-500`}
+    >
+      <img src={src} alt="profile_image" className={`${size}  rounded-full `} />
+      <span>{username}</span>
+    </div>
+  );
+};
 
-const Profile = ({ src, username, size = "w-10 h-10" }: { src: string, username: string, size?: string }) => {
-    const navigate = useNavigate();
-    return (
-        <div onClick={() => navigate(`/profile/${username}`)} className='cursor-pointer flex items-center gap-2 text-zinc-500'>
-            <img src={src} alt="profile_image" className={`${size}  rounded-full `} />
-            <span>
-                {username}
-            </span>
-        </div>
-    )
-}
-
-export default Profile
+export default Profile;
